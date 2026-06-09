fdd39eb05f3a4fdeef509c5a7e67c410ba8539aa
Great Lucky <lgreat098@gmail.com>
2026-06-09 17:50:37 +0100

    implement footer

--- app/page.tsx
+++ app/page.tsx
@@ -51,8 +51,10 @@ export default function Home() {
             </h1>
             <p className="max-w-3xl text-lg leading-8 text-[#29292B]">
               Ask, discuss, and discover expert insights in one place.
-              AskWinFlow transforms conversations <br /> into a searchable
-              knowledge base through structured discussions,
+              AskWinFlow transforms
+              <br />
+              conversations into a searchable knowledge base through structured
+              discussions,
               <br /> attachments, and community-driven ratings.
             </p>
             <button className="flex items-center gap-2 bg-[#A10F05] text-white px-6 py-3 rounded-md font-medium">
@@ -89,7 +91,7 @@ export default function Home() {
       <section>
         <div className="flex flex-col gap-8 items-center mt-20">
           <h1 className="font-bold text-3xl">How It Works</h1>
-          <p className="max-w-3xl text-lg leading-8 text-[#29292B]">
+          <p className="max-w-3xl text-center text-lg leading-8 text-[#29292B]">
             A simple process designed to help you ask better questions,
             collaborate
             <br /> with experts, and find trusted insights faster.
@@ -123,6 +125,206 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section>
+        <div className="flex flex-col gap-2 items-center mt-20">
+          <h1 className="font-bold text-3xl">
+            Got Questions?
+            <span className="text-[#A10F05]"> We’ve Got Answers</span>
+          </h1>
+          <p className="max-w-3xl text-center text-lg leading-8 text-[#29292B]">
+            Everything you need to know about AskWinFlow from getting started
+            <br />
+            to advanced collaboration with experts
+          </p>
+        </div>
+        <div className="space-y-2 m-5 pb-5">
+          <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50">
+              <span>What is AskWinFlow?</span>
+              <svg
+                className="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </summary>
+            <div className="p-4">
+              <p className="text-gray-700">
+                AskWinFlow is a structured knowledge-sharing platform where
+                professionals, educators, and tech enthusiasts can ask
+                questions, share ideas, participate in discussions, and discover
+                valuable insights from a growing community
+              </p>
+            </div>
+          </details>
+          <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50">
+              <span>How is AskWinFlow different from chat groups?</span>
+
+              <svg
+                className="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </summary>
+
+            <div className="p-4">
+              <p className="text-gray-700">
+                Unlike traditional chat platforms where conversations quickly
+                get buried, AskWinFlow organizes discussions into searchable
+                threads, making it easy to find and revisit valuable knowledge
+                whenever you need it.
+              </p>
+            </div>
+          </details>
+
+          <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50">
+              <span> Who can use AskWinFlow?</span>
+
+              <svg
+                className="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </summary>
+
+            <div className="p-4">
+              <p className="text-gray-700">
+                AskWinFlow is designed for professionals, educators, students,
+                product managers, developers, HR specialists, and anyone looking
+                to learn, share expertise, or collaborate with others
+              </p>
+            </div>
+          </details>
+          <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50">
+              <span>Can I attach files to my questions or ideas?</span>
+
+              <svg
+                className="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </summary>
+
+            <div className="p-4">
+              <p className="text-gray-700">
+                Yes. You can add supporting documents, images, presentations,
+                and other relevant attachments to provide context and help
+                others better understand your post.
+              </p>
+            </div>
+          </details>
+          <details className="group [&amp;_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-white px-4 py-3 font-medium text-gray-900 hover:bg-gray-50">
+              <span>Is AskWinFlow free to use?</span>
+
+              <svg
+                className="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </summary>
+
+            <div className="p-4">
+              <p className="text-gray-700">
+                Yes. The MVP version of AskWinFlow is free to join, allowing
+                users to ask questions, share ideas, engage in discussions, and
+                access community knowledge.
+              </p>
+            </div>
+          </details>
+        </div>
+      </section>
+      <footer>
+        <div className="bg-[#111111] p-5">
+          <div className="flex justify-between items-center p-5">
+            <div className="flex flex-col items-center cursor-pointer">
+              <Image
+                src="/askwinflow_logo.png"
+                alt="AskWinFlow Logo"
+                width={25}
+                height={25}
+              />
+              <span className="font-semibold cursor-pointer invert">
+                AskWinFlow
+              </span>
+              <p className="invert">
+                Transforming questions,
+                <br /> ideas, and discussions
+                <br /> into lasting knowledge
+              </p>
+            </div>
+            <div className="invert">
+              <p className="font-bold">Links</p>
+              <ul className="gap-10">
+                <li>Home</li>
+                <li>Features</li>
+                <li>How it Works</li>
+              </ul>
+            </div>
+            <div className="flex justify-between gap-3">
+              <button className="px-6 py-3 rounded-md bg-[#FF907E33] text-[#EEEEF0]">
+                Get Experts Insights
+              </button>
+              <button className="flex items-center px-6 py-3 gap-2 rounded-md text-center text-[#FFF9F9] bg-[#A10F05] ">
+                Subscribe
+                <ArrowRight />
+              </button>
+            </div>
+          </div>
+          <div className="border-t invert">
+            <p className="text-white p-3 invert">
+              2026 copyright by AskWinFlow. All Right Reserved
+            </p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
